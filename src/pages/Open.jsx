7d6e72ef7baa883ad3a34f392b00503b5156667f
SSJDeathSpawn <adikesavan28@gmail.com>
2024-09-21 18:02:07 +0530

    FIXME: Open dialog box freezes app

--- src/pages/Open.jsx
+++ src/pages/Open.jsx
@@ -1,4 +1,29 @@
+import { open } from "@tauri-apps/api/dialog";
+import { homeDir } from "@tauri-apps/api/path";
+import { useEffect } from "react"
+
 export default function Open() {
+    
+    useEffect(() => {
+        // const loadFile = async () => {
+        //     const file = await invoke('open_file', {});
+        //     console.log(file);
+        // };
+        const loadFile = async () => {
+            const file = await open({
+                title: "Open PDF",
+                filters: [{
+                    name: "PDF Document",
+                    extensions: ['pdf']
+                }],
+                defaultPath: await homeDir()
+            });
+            console.log(file);
+        }
+
+        loadFile();
+    }, [])
+
     return (
         <div className='w-[80%] self-center h-full flex flex-col gap-[10%]'>
             <h3 className='text-5xl text-center'>
